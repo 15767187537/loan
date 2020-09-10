579e1817d16ef74db7833c6049fba90d195ad70f
xztx <15767187537@163.com>
2020-09-10 21:53:48 +0800

    增加loan模块的基本启动类和文件

---
+++ xztx-config/src/main/java/com/xztx/XztxConfigApplication.java
@@ -0,0 +1,13 @@
+package com.xztx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XztxConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XztxConfigApplication.class, args);
+    }
+
+}
